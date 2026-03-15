d2e624d4874c0da1c8ee654f6f2c972a29162677
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-03-15 12:53:31 +0600

    Example of code that violates SRP

---
+++ SRP/AnotherExample/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP.AnotherExample
+{
+    public class Order
+    {
+        public int orderID {  get; set; }
+        public string orderName { get; set; }
+    }
+}
---
+++ SRP/AnotherExample/OrderService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP.AnotherExample
+{
+    
+    public class OrderService
+    {
+        public void CreateOrder(Order order )
+        {
+           SaveToDatabase( order );
+            SendEmail(order);
+            Log(order);
+        }
+        private void SaveToDatabase( Order order )
+        {
+            Console.WriteLine("saving order");
+        }
+        private void SendEmail(Order order)
+        {
+            Console.WriteLine("sending email");
+        }
+        private void Log(Order order)
+        {
+            Console.WriteLine("logging order");
+        }
+    }
+}

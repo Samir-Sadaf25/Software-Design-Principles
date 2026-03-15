9046ea63e1ed7574fcdbeca9fa4a5b77e8b046c1
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-03-15 14:05:40 +0600

    example of code that violates OCP

---
+++ OCP/DiscountCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP
+{
+    public class DiscountCalculator
+    {
+        public double CalculateDiscount(string customerType,double amount)
+        {
+            if (customerType == "Regular")
+                return amount * 0.05;
+
+            if (customerType == "Premium")
+                return amount * 0.10;
+
+            return 0;
+        }
+    }
+}
+/*
+ Every time a new customer type appears:
+VIP
+Gold
+Platinum
+i must need to modify this class and that violates OCP
+*/

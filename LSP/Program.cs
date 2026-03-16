42ebe476a8dbcf7d3a6f79e128bde2d2f41a714c
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-03-16 16:03:50 +0600

    example of code that violates LSP

---
+++ LSP/Program.cs
@@ -0,0 +1,5 @@
+﻿using LSP;
+
+Square square = new Square();
+square.height = 10;
+square.width = 10;
---
+++ LSP/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP
+{
+    public class Rectangle
+    {
+        public int width {  get; set; }
+        public int height { get; set; }
+        public int CalculateArea()
+        {
+            return width * height;
+        }
+    }
+}

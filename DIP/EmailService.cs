2ba98269b71faa794602947ab6a17ea2f14b9583
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-03-18 20:51:16 +0600

    Example: violates Dependency Inversion Principle

---
+++ DIP/EmailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIP
+{
+    public class EmailService
+    {
+        public void Send()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DIP/OrderService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIP
+{
+     public class OrderService
+    {
+        private EmailService _emailService = new EmailService();
+        public void placeOrder()
+        {
+            Console.WriteLine("Order placed");
+            _emailService.Send();
+        }
+    }
+}

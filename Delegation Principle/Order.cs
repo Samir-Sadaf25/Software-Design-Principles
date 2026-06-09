1b8290879720a2ea06ab239fcd04f7656d185d66
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-06-09 17:00:57 +0600

    understand delegation principle

---
+++ Delegation Principle/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace Delegation_Principle
+{
+    public class Order
+    {
+        public int Price { get; set; }
+        public int valueAddedTax { get; set; }
+        public int total => Price + valueAddedTax;
+    }
+}

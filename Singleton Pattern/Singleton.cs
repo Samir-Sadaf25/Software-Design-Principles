83a7aeb6ca5285a930232853f6c647e5421daaba
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-06-09 22:17:14 +0600

    understand singleton pattern

---
+++ Singleton Pattern/Singleton.cs
@@ -0,0 +1,19 @@
+﻿namespace Singleton_Pattern
+{
+    public class Singleton
+    {
+        public static Singleton? _instance;
+        private Singleton()
+        {
+            
+        }
+        public static Singleton CreateInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+}

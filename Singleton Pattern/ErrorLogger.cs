e7f72461f63a1033bcb4b05869b845309a7d99dd
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-06-09 22:31:12 +0600

    add practical example of Singleton Pattern

---
+++ Singleton Pattern/ErrorLogger.cs
@@ -0,0 +1,23 @@
+﻿namespace Singleton_Pattern
+{
+    public class ErrorLogger
+    {
+        public static ErrorLogger? _instance;
+        private ErrorLogger()
+        {
+            
+        }
+        public static ErrorLogger CreateLogger()
+        {
+            if (_instance == null)
+            {
+                _instance = new ErrorLogger();
+            }
+            return _instance;
+        }
+        public void LogError(string message)
+        {
+            Console.WriteLine($"error : {message}");
+        }
+    }
+}

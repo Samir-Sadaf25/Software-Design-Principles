22b5cf8a2d0455be83fb2861c8067c79e3c5dbd3
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-06-10 16:39:31 +0600

    understand shallow copy vs deep copy in C#

---
+++ Prototype Pattern/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_Pattern
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public List<string> Skills { get; set; }
+
+       
+    }
+}
---
+++ Prototype Pattern/Program.cs
@@ -0,0 +1,6 @@
+﻿using Prototype_Pattern;
+
+Employee emp1 = new Employee { Name = "samir", Skills = ["c#", ".Net"] };
+Employee emp2 = emp1;
+emp2.Name = "Rahim";
+Console.WriteLine(emp1.Name);

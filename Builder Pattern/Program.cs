1cf3ad82fe3b141f74296e7225592c889b594cc5
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-06-10 20:12:02 +0600

    Learn Builder Design Pattern

---
+++ Builder Pattern/Program.cs
@@ -0,0 +1,10 @@
+﻿using Builder_Pattern;
+
+var user = new UserBuilder()
+    .setFirstName("Sadaf Hossain")
+    .setLastName("Samir")
+    .setEmail("samir@gmail.com")
+    .Build();
+Console.WriteLine(user.FirstName);
+Console.WriteLine(user.LastName);
+Console.WriteLine(user.Email);
---
+++ Builder Pattern/UserBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder_Pattern
+{
+    public class UserBuilder
+    {
+        private readonly  User _user = new();
+        public UserBuilder setFirstName(string firstName)
+        {
+            _user.FirstName = firstName;
+            return this;
+        }
+        public UserBuilder setLastName(string lastName)
+        {
+            _user.LastName = lastName;
+            return this;
+        }
+        public UserBuilder setEmail(string email)
+        {
+            _user.Email = email;
+            return this;
+        }
+        public User Build()
+        {
+            return _user;
+        }
+    }
+}
